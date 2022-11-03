dd04fecdb159e0af40bb0d1c938f5a5bb3ca519f
Priti Kumari <75195316+priti27599@users.noreply.github.com>
2022-11-03 10:33:33 +0530

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("read").addEventListener('click', function () {
+    // document.getElementById("cir").style.display = "none";
+    document.getElementById("act").style.backgroundColor = "#fff";
+    document.getElementById("act1").style.backgroundColor = "#fff";
+    document.getElementById("act2").style.backgroundColor = "#fff";
+    document.getElementById("cir1").style.display = "none";
+    document.getElementById("cir2").style.display = "none";
+    document.getElementById("cir3").style.display = "none";
+    document.getElementById("nu").innerHTML = "0";
+})
